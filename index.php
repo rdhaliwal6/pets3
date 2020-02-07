d074e57d5ded3a5ce5668f80a53012ba2b05af8e
Rajpreet Dhaliwal <Rajpreet9910@gmail.com>
2020-02-07 02:59:59 -0500

    finshished pets3

--- index.php
+++ index.php
@@ -1,70 +1,83 @@
 <?php
 session_start();
-
-// turn on error reporting
-ini_set('display_errors', 1);
+// Turn on error reporting -- this is critical!
+ini_set('display_errors',1);
 error_reporting(E_ALL);
 
-// require autoload file
+//Require autoload file
 require("vendor/autoload.php");
+require_once ("model/validate-functions.php");
 
-// Instantiate F3
-$f3 = Base::instance();
-$f3 -> set('colors', array('pink', 'green', 'blue'));
-
-//set debug level
+//Instantiate F3
+$f3 = Base::Instance();
+$f3->set('colors', array('pink', 'green', 'blue'));
 $f3->set('DEBUG', 3);
 
-// Define a default route
-$f3->route('GET /', function () {
-    echo "<h1> My Pets <br></h1>";
+
+//Define a default route
+$f3->route("GET /", function (){
+    echo "<h1>My Pets</h1>";
     echo "<a href='order'>Order a Pet</a>";
-    //$views = new Template();
-    //echo $views->render("views/home.html");
 });
 
-$f3->route('GET /order', function() {
-    $view = new Template();
-    echo $view->render('views/form1.html');
-});
-
-$f3->route('POST /order2', function() {
-//    var_dump($_POST);
-    $_SESSION['pets'] = $_POST['animalName'];
-    $view = new Template();
-    echo $view->render('views/form2.html');
-});
-
-$f3->route('POST /results', function() {
-//    var_dump($_POST);
-    $_SESSION['pets2'] = $_POST['color'];
-    $view = new Template();
-    echo $view->render('views/results.html');
-});
-
-$f3->route('GET /@item', function($f3, $params){
-    //var_dump($params);
-    $item = $params['item'];
-
-    switch($item) {
+$f3->route("GET /@animal", function($f3, $params) {
+    $animal = $params['animal'];
+    switch ($animal) {
         case 'chicken':
-            echo "<p>Cluck</p>";
+            echo "Cluck!";
             break;
         case 'dog':
-            echo "<p>woof</p>";
-            break;
-        case 'horse':
-            echo "<p>neigh</p>";
+            echo "Woof!";
             break;
         case 'cat':
-            echo "<p>meow</p>";
+            echo "Meow!";
             break;
-        case 'dino':
-            echo "<p>rwar</p>";
+        case 'horse':
+            echo "Nay!";
+            break;
+        case 'cow':
+            echo "Moo!";
             break;
         default:
             $f3->error(404);
     }
 });
-// Run F3
+
+$f3->route("GET|POST /order", function($f3) {
+    $_SESSION = array();
+    if(isset($_POST['animal'])) {
+        $animal = $_POST['animal'];
+        if(validString($animal)) {
+            $_SESSION['animal'] = $animal;
+            $f3->reroute('/order2');
+        } else {
+            $f3->set("errors['animal']", "Please enter an animal.");
+        }
+    }
+    $views = new Template();
+    echo $views->render('views/form1.html');
+});
+
+$f3->route("GET|POST /order2", function($f3) {
+    // var_dump($_POST);
+    if(isset($_POST['color'])) {
+        $color = $_POST['color'];
+        if(validColor($color)) {
+            $_SESSION['color'] = $color;
+            $f3->reroute('/results');
+        } else {
+            $f3->set("errors['color']", "Please select a color.");
+        }
+    }
+    $views = new Template();
+    echo $views->render('views/form2.html');
+});
+
+$f3->route("GET|POST /results", function() {
+    //var_dump($_POST);
+    $views = new Template();
+    echo $views->render('views/results.html');
+});
+
+//Run f3
 $f3->run();
